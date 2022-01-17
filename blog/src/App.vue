a685fae7073e544982ef5f3bb067259516137a10
eden <goodeden3@gmail.com>
2022-01-17 10:20:10 +0900

    install router

---
+++ blog/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <Nav />
+
+  <div class="container mt-4">
+    <h5>React 개발자의 블로그입니다</h5>
+    <p>- Vue로 만들었음 -</p>
+  </div>
+
+  <List :item="item"/>
+</template>
+
+<script>
+import Nav from '@/components/Nav';
+import List from '@/components/List';
+import item from './assets/blog.js';
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      item : item,
+    }
+  },
+  components: {
+    Nav,
+    List,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  /*margin-top: 60px;*/
+}
+</style>
---
+++ blog/src/components/List.vue
@@ -0,0 +1,26 @@
+<template>
+  <div v-for="(item, i) in item" :key="i">
+    <h5>{{ item.title }}</h5>
+    <p>{{ item.date }}</p>
+  </div>
+</template>
+
+<script>
+// import item from '../assets/blog';
+
+export default {
+  name: 'List',
+  data() {
+    return {
+      // item : item,
+    }
+  },
+  props: {
+    item : Array,
+  }
+}
+</script>
+
+<style>
+
+</style>
